339c8371028dec01f485ae228957395b995feb54
Ander Sanjuan <50419284+Andersanjuan@users.noreply.github.com>
2022-11-09 21:05:45 -0500

    Update identifyCrisprSite.sh

--- identifyCrisprSite.sh
+++ identifyCrisprSite.sh
@@ -1,25 +1,27 @@
-#!/bin/bash
+ #!/bin/bash
+#using genes from exomename_topmotifs.fasta files in the crispr_ready folder identify CRISPR sites
+#Find the sequences which contain "NGG", where "N" can be any base, that has at least 20 bps upstream
+#output headers and sequences to a new file {exome_name}_precrispr.fasta
 
-rm -f *_precrispr.fasta
+rm -f *_precrispr.fasta #removes precrispr.fasta file if it already exists
 
-for i in `ls *topmotif.fasta`
+for i in `ls *topmotif.fasta` #for loop looking at all the topmotif.fasta files
 do
-	exomename=`echo $i | cut -d'_' -f1`
-	echo $exomename
-	grep 'CGG' $i | awk -F"CGG" '{ print $1 }' | awk '{ print $0,length }' > out.txt
+	exomename=`echo $i | cut -d'_' -f1` #gets the name of the animal
+	echo $exomename #checks animal name that we are working with
+	grep '[ATCG]GG' $i | awk -F"[ATCG]GG" '{ print $1 }' | awk '{ print $0,length }' > out.txt #looks for [ATCG]GG pattern and gives the length of basepairs
 
-	while read line
+	while read line #reads line by line of selected topmotif fasta file with crispr site
 	do
 
-	l=`echo "$line" | cut -d' ' -f2`
-	d=`echo "$line" | cut -d' ' -f1`
+	l=`echo "$line" | cut -d' ' -f2` #gets length of line with the crispr site
+	d=`echo "$line" | cut -d' ' -f1` #takes baspairs left of the [ATCG]GG pattern
 
-	if [ $l -ge 20 ]
+	if [ $l -ge 20 ] #if there are 20 bps upstream of the [ATCG]GG pattern, greater or equal to 20
 	then
-		grep -B 1 "${d}CGG" $i >> ${exomename}_precrispr.fasta
+		grep -B 1 "${d}[ATCG]GG" $i >> ${exomename}_precrispr.fasta #outputs header and sequences of genes with crispr sites into new fasta file
 	fi
 done < out.txt
-
 
 done
 
